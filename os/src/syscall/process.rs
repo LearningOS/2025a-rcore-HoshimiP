b78d68b222837a398ca031bb81fc834a7e764384
HoshimiP <ryoken@LAPTOP-0J8P1DQ3.localdomain>
2025-10-29 13:59:42 +0800

    lab3

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -243,10 +243,16 @@ pub fn sys_spawn(path: *const u8) -> isize {
 }
 
 // YOUR JOB: Set task priority.
-pub fn sys_set_priority(_prio: isize) -> isize {
+pub fn sys_set_priority(prio: isize) -> isize {
     trace!(
-        "kernel:pid[{}] sys_set_priority NOT IMPLEMENTED",
+        "kernel:pid[{}] sys_set_priority",
         current_task().unwrap().pid.0
     );
-    -1
+    if prio < 2 {
+        return -1;
+    }
+    let task = current_task().unwrap();
+    let mut inner = task.inner_exclusive_access();
+    inner.stride = prio as usize;
+    prio
 }
